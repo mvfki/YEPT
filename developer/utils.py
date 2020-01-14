5171d59d43dba9fb537a1c399663757dc755e38b
Yichen W <wangych@bu.edu>
2020-01-14 12:50:10 +0800

    Clean up useless func

--- developer/utils.py
+++ developer/utils.py
@@ -2,51 +2,28 @@
 import pickle
 from Bio import SeqIO
 import os
-import sqlite3
 
 this_dir, this_filename = os.path.split(__file__)
 
-def importAnn(src = 'contig'):
-    ann = os.path.join(this_dir, src+'.ann.pkl')
-    with open(ann, 'rb') as f:
-        table = pickle.load(f)
-    f.close()
-    return table
-
-def importGenome(species):
-    dataPath = os.path.join(this_dir, 'data', species, 'seq.pkl')
-    with open(dataPath, 'rb') as f:
-        fasta = pickle.load(f)
-    f.close()
-    return fasta
-
-def search(species, keyword):
+def formatFasta(file_name, species, save = True):
     '''
-    Search the preprepared SQLite3 database for the given keywords, and return 
-    the matched result.
+    Load the text FASTA format file into a pickled data file, in the form of a dict
     Arguments:
     ----------
-    species - str. 
-              Choose from {'P', 'C', 'J', 'O'}.
-    keyword - str.
-              Can be either GenBank accession ID, gene symbol, or systematic 
-              ID (PomBase) for S. pombe. 
-    Return:
+    file_name - str
+    species   - str, choose from {'P', 'J', 'O', 'C'}
+    save      - bool, default True
+    Returns:
     ----------
-    result - list.
-             elements are tuples with 9 fields each, where the 3rd is the 
-             chromosome identifier, the 4th for the start, the 5th for the end, 
-             the 6th for the strand. 
+    fasta - dict
+            With sequence names being the keys and the corresponding sequences being the values.
     '''
-    sp = {'P': 'S_pombe'}
-    dbPath = os.path.join(this_dir, 'data', sp[species], 'ANNOTATION.db')
-    conn = sqlite3.connect(dbPath)
-    c = conn.cursor()
-    cmd = '''SELECT * 
-    FROM ANNOTATION
-    WHERE GenBank LIKE "%{0}%" 
-    OR PomBase LIKE "%{0}%" 
-    OR Symbol LIKE "%{0}%";'''.format(keyword)
-    return c.execute(cmd).fetchall()
-
-    
+    assert species in {'P', 'J', 'O', 'C'}
+    destination = os.path.join(this_dir, species, 'seq.pkl')
+    records = SeqIO.parse(file_name, 'fasta')
+    fasta = {r.name: r.seq for r in records}
+    if save:
+        with open(destination, 'wb') as out:
+            pickle.dump(fasta, out)
+        out.close
+    return fasta
